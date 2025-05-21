e7b7ea5ff56eed6b49cfb3b41eda8467fb5ebc62
luis angel salas laza <angel1.lasl46@gmail.com>
2025-05-21 11:58:03 -0500

    sda

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -4,4 +4,13 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';  // Asegúrate de esta línea
+import '@/assets/lib/fontawesome-v6.5.2/css/all.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-light.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-regular.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-solid.css'
+import '@/assets/lib/fontawesome-v6.5.2/css/sharp-thin.css'
+
 createApp(App).use(store).use(router).mount('#app')
