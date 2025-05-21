e97c969e5365eb4e30fe9c603633c31aa4650df5
luis angel salas laza <angel1.lasl46@gmail.com>
2025-05-21 16:42:44 -0500

    rerer

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -4,9 +4,14 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// LIBRERÍAS
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';  // Asegúrate de esta línea
+import 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+
 import '@/assets/lib/fontawesome-v6.5.2/css/all.css'
 import '@/assets/lib/fontawesome-v6.5.2/css/sharp-light.css'
 import '@/assets/lib/fontawesome-v6.5.2/css/sharp-regular.css'
@@ -14,7 +19,8 @@ import '@/assets/lib/fontawesome-v6.5.2/css/sharp-solid.css'
 import '@/assets/lib/fontawesome-v6.5.2/css/sharp-thin.css'
 
 
-//PERSONALIZADO ASIDE
+//PERSONALIZADO
+import '@/assets/css/font.css'
 import '@/assets/css/main.css'
 
 
