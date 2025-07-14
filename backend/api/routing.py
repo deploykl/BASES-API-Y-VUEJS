91a34c512b9186c3fbda43215c10fa262959e624
Luis Salas <angel1.lasl46@gmail.com>
2025-07-13 20:52:21 -0500

    yes

--- backend/api/routing.py
+++ backend/api/routing.py
@@ -4,12 +4,3 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r"ws/chat/$", consumers.ChatConsumer.as_asgi()),
 ]
-
-from django.urls import re_path
-from . import consumers
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
-]
