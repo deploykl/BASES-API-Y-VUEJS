91648663023ca8e3320c17c619d4b218f33dcfab
luis angel salas laza <angel1.lasl46@gmail.com>
2025-07-24 16:29:00 -0500

    erer

--- backend/api/user/models.py
+++ backend/api/user/models.py
@@ -17,7 +17,30 @@ def user_image_path(instance, filename):
 class User(AbstractUser):
     image = models.ImageField(upload_to=user_image_path, default="img/empty.png", null=True, blank=True)
     genero = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name="Género", null=True, blank=True)
-    celular = models.CharField(max_length=20, null=True, blank=True, verbose_name="Celular", null=True, blank=True)
+    celular = models.CharField(max_length=20, null=True, blank=True, verbose_name="Celular")
+    distrito = models.CharField(max_length=20, null=True, blank=True, verbose_name="Distrito")
+    departamento = models.CharField(max_length=20, null=True, blank=True, verbose_name="Departamento")
+
+
+    # Campos del establecimiento laboral
+    #codigo = models.CharField(max_length=8, null=True, blank=True, verbose_name="Código de establecimiento")
+    #institucion = models.CharField(max_length=8, null=True, blank=True, verbose_name="Institución")
+    #establecimiento = models.CharField(max_length=200, null=True, blank=True, verbose_name="Nombre del establecimiento")
+    #departamento_es = models.CharField(max_length=100, null=True, blank=True, verbose_name="Departamento del establecimiento")
+    #provincia_es = models.CharField(max_length=100, null=True, blank=True, verbose_name="Provincia del establecimiento")
+    #distrito_es = models.CharField(max_length=100, null=True, blank=True, verbose_name="Distrito del establecimiento")
+    #disa = models.CharField(max_length=100, null=True, blank=True, verbose_name="DISA de salud")
+    #cod_disa = models.CharField(max_length=20, null=True, blank=True, verbose_name="Código DISA")
+    #red = models.CharField(max_length=100, null=True, blank=True, verbose_name="Red de salud")
+    #cod_red = models.CharField(max_length=20, null=True, blank=True, verbose_name="Código Red")
+    #cod_microred = models.CharField(max_length=20, null=True, blank=True, verbose_name="Código Microred")
+    #unidad_ejecutora = models.CharField(max_length=100, null=True, blank=True, verbose_name="Unidad Ejecutora")
+    #cod_ue = models.CharField(max_length=20, null=True, blank=True, verbose_name="Código Unidad Ejecutora")
+    #categoria = models.CharField(max_length=20, null=True, blank=True, verbose_name="Categoría del establecimiento")
+    #direccion = models.CharField(max_length=250, null=True, blank=True, verbose_name="Dirección del establecimiento")
+    #norte = models.CharField(max_length=100, null=True, blank=True, verbose_name="Dirección del establecimiento")
+    #este = models.CharField(max_length=100, null=True, blank=True, verbose_name="Dirección del establecimiento")
+
 
     def save(self, *args, **kwargs):
         """Maneja la imagen por defecto para nuevos usuarios"""
